21228aa49b162a79ab01666b3d6b4fc66012dfb1
Erik Martinez <emrbeltane@gmail.com>
2018-07-29 22:31:03 +0200

    Pathfinder small improvement

--- version0/Pathfinder.java
+++ version0/Pathfinder.java
@@ -12,40 +12,37 @@ public class Pathfinder {
         uc = manager.uc;
     }
 
-    //*************PATHFINDING*********************//
-
     final int INF = 1000000;
 
-    boolean rotateRight = true; //if I should rotate right or left
-    Location lastObstacleFound = null; //latest obstacle I've found in my way
-    int minDistToEnemy = INF; //minimum distance I've been to the enemy while going around an obstacle
-    Location prevTarget = null; //previous target
+    boolean rotateRight = true; // Should rotate right or left
+    Location lastObstacleFound = null; // Latest obstacle found
+    int minDistToEnemy = INF; // Minimum distance while going around an obstacle
+    Location prevTarget = null; // Previous target
+    int counter = 0;
 
     public void moveTo(Location target){
-        //No target / Can't move? ==> bye!
         if (!uc.canMove()) return;
         if (target == null) return;
 
-        //different target? ==> previous data does not help!
+        // Different target? ==> previous data does not help!
         if (prevTarget == null || !target.isEqual(prevTarget)) resetPathfinding();
 
-        //If I'm at a minimum distance to the target, I'm free!
+        // If minimum distance to the target, reset
         int d = manager.myLocation.distanceSquared(target);
         if (d <= minDistToEnemy) resetPathfinding();
 
-        //Update data
+        // Update data
         prevTarget = target;
         minDistToEnemy = Math.min(d, minDistToEnemy);
 
-        //If there's an obstacle I try to go around it [until I'm free] instead of going to the target directly
+        // If there's an obstacle, try to go around it instead of going to the target directly
         Direction dir = manager.myLocation.directionTo(target);
         if (lastObstacleFound != null) dir = manager.myLocation.directionTo(lastObstacleFound);
 
-        //This should not happen for a single unit, but whatever
+        // This should not happen for a single unit, but whatever
         if (uc.canMove(dir)) resetPathfinding();
 
-        //I rotate clockwise or counterclockwise (depends on 'rotateRight'). If I try to go out of the map I change the orientation
-        //Note that we have to try at most 16 times since we can switch orientation in the middle of the loop. (It can be done more efficiently)
+        // Rotate clockwise or counterclockwise. If try to go out of the map change the orientation
         for (int i = 0; i < 16; ++i){
             if (uc.canMove(dir)){
                 uc.move(dir);
@@ -55,12 +52,17 @@ public class Pathfinder {
             if (uc.isOutOfMap(newLoc)) {
                 rotateRight = !rotateRight;
             } else {
-                //If I could not go in that direction and it was not outside of the map, then this is the latest obstacle found
+                // Update latest obstacle found and check counter for unit obstacles
                 Location possibleObstacle = manager.myLocation.add(dir);
-                if (uc.senseUnit(possibleObstacle) == null){
+                if (uc.senseUnit(possibleObstacle) == null || counter > 1){
                     lastObstacleFound = manager.myLocation.add(dir);
                     if (rotateRight) dir = dir.rotateRight();
                     else dir = dir.rotateLeft();
+                } else {
+                    counter++;
+                    if (counter > 10) {
+                        resetPathfinding();
+                    }
                 }
             }
         }
@@ -68,7 +70,7 @@ public class Pathfinder {
         if (uc.canMove(dir)) uc.move(dir);
     }
 
-    //clear some of the previous data
+    // Clear some of the previous data
     private void resetPathfinding(){
         lastObstacleFound = null;
         minDistToEnemy = INF;
